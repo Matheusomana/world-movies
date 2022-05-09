3ff07d44fe70d0314abf33cb8e4fba6650779ecb
Prascos <matheusomana@gmail.com>
2022-05-09 15:09:08 -0300

    atualizacoes do projeto - etapa 2

---
+++ src/pages/Home/index.js
@@ -0,0 +1,24 @@
+function Home() {
+    return(
+        <div>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg" alt="Spider Man"/>
+                    <span>Spider Man</span>
+                </li>
+                <li>
+                    <img src="https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg" alt="Spider Man"/>
+                    <span>Spider Man</span>
+                </li>
+                <li>
+                    <img src="https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg" alt="Spider Man"/>
+                    <span>Spider Man</span>
+                </li>
+            </ul>
+        </div>
+       
+    )
+}
+
+export default Home;
